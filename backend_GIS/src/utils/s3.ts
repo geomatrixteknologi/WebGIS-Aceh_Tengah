1a1ede59b7f13bfbe740b8858a1e1a3bcf479d6a
Geomatrix Teknologi <geomatrixteknologi@gmail.com>
2025-07-12 23:53:44 +0700

    perbaikan di utility s3

--- backend_GIS/src/utils/s3.ts
+++ backend_GIS/src/utils/s3.ts
@@ -1,4 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
